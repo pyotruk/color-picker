c00b1364d44d5b7b17eb5b0a5335bad8e3bbe12a
Vladimir Ivanenko <hi@vldmr.me>
2023-01-19 22:14:51 +0400

    hex color calculation added

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -2,14 +2,18 @@ import React, {useRef, useEffect, useState} from "react";
 import "./Canvas.scss";
 import PickerSquare from "./../PickerSquare/PickerSquare";
 
+interface Position {
+  x: number;
+  y: number;
+}
+
 export default function Canvas(props: {
   setColor: (color: string) => void,
 }) {
   const canvas = useRef<HTMLCanvasElement>();
   const ctx = useRef<null | CanvasRenderingContext2D>();
 
-  const [x, setX] = useState<number>(0);
-  const [y, setY] = useState<number>(0);
+  const [position, setPosition] = useState<Position>({x: 0, y: 0});
 
   const initCanvas = (): void => {
     canvas.current = document.getElementById("canvas") as HTMLCanvasElement;
@@ -37,30 +41,43 @@ export default function Canvas(props: {
     drawImage();
   }, []);
 
-  const getMousePosition = (event: React.MouseEvent) => {
-    const clientRect = canvas.current?.getBoundingClientRect();
-    if (!clientRect) {
-      throw new Error("clientRef is null.");
+  const getPosition = (event: React.MouseEvent): Position => {
+    if (!canvas.current) {
+      return {x: 0, y: 0};
     }
+    const clientRect = canvas.current.getBoundingClientRect();
     return {
       x: event.clientX - clientRect.left,
       y: event.clientY - clientRect.top,
     };
   }
 
+  const rgbToHex = (r: number, g: number, b: number) => {
+    if (r > 255 || g > 255 || b > 255) {
+      throw new Error("Invalid color component");
+    }
+    return ((r << 16) | (g << 8) | b).toString(16);
+  }
+
+  const getHexColor = (position: Position): string => {
+    if (!ctx.current) {
+      return "";
+    }
+    const colorData = ctx.current?.getImageData(position.x, position.y, 1, 1).data;
+    return "#" + ("000000" + rgbToHex(colorData[0], colorData[1], colorData[2])).slice(-6);
+  }
+
   const handleMouseMove = (event: React.MouseEvent) => {
-    const pos = getMousePosition(event);
-    setX(pos.x);
-    setY(pos.y);
-    const color = `X = ${pos.x}, Y = ${pos.y}`;
-    props.setColor(color);
+    const position = getPosition(event);
+    setPosition(position);
+    props.setColor(getHexColor(position));
   }
 
   return (
     <div className="Canvas">
       <canvas id="canvas" onMouseMove={handleMouseMove}></canvas>
       <img id="img" src="/img.jpg" width="1920" height="1080"/>
-      <PickerSquare x={x} y={y}/>
+      <PickerSquare x={position.x} y={position.y}/>
     </div>
   );
 }

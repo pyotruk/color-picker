adf4a9c128411f85acbee3848f496dd8360ad0c1
Vladimir Ivanenko <hi@vldmr.me>
2023-01-20 22:41:38 +0400

    zoom circle added

--- src/components/PickerSquare/PickerSquare.tsx
+++ src/components/PickerSquare/PickerSquare.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./PickerSquare.scss";
 import {ColorMatrix, Position} from "../../structures/geometry";
-import {PICKER_SQUARE_SIZE} from "../../structures/constants";
+import {PICKER_SQUARE_SIZE, ZOOMED_PIXEL_SIZE} from "../../structures/constants";
 
 export default function PickerSquare(props: {
   centerPosition: Position,
@@ -12,23 +12,41 @@ export default function PickerSquare(props: {
     <div
       className="PickerSquare"
       style={{
-        width: `${PICKER_SQUARE_SIZE}px`,
-        height: `${PICKER_SQUARE_SIZE}px`,
-        top: `${props.centerPosition.y - PICKER_SQUARE_SIZE/2}px`,
-        left: `${props.centerPosition.x - PICKER_SQUARE_SIZE/2}px`,
+        top: `${props.centerPosition.y - PICKER_SQUARE_SIZE / 2}px`,
+        left: `${props.centerPosition.x - PICKER_SQUARE_SIZE / 2}px`,
       }}
     >
-      {props.colorMatrix.map((row: string[], key: number) => {
-        return <div className="row" key={key}>
-          {row.map((color: string, key: number) => {
-            return <div
-              className="px"
-              key={key}
-              style={{backgroundColor: color}}
-            ></div>
-          })}
-        </div>;
-      })}
+      <div
+        className="zoom"
+        style={{
+          width: `${PICKER_SQUARE_SIZE}px`,
+          height: `${PICKER_SQUARE_SIZE}px`,
+        }}
+      >
+        <i/>
+
+        {props.colorMatrix.map((row: string[], key: number) => {
+          return <div
+            className="row"
+            key={key}
+            style={{
+              height: `${ZOOMED_PIXEL_SIZE}px`,
+              lineHeight: `${ZOOMED_PIXEL_SIZE}px`,
+            }}
+          >
+            {row.map((color: string, key: number) => {
+              return <div
+                className="px"
+                key={key}
+                style={{
+                  backgroundColor: color,
+                  width: `${ZOOMED_PIXEL_SIZE}px`,
+                }}
+              ></div>
+            })}
+          </div>;
+        })}
+      </div>
     </div>
   );
 }
